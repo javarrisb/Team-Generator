1711b8dc07d5bde8ba72d0b7ff708eace1a08b55
Javarris Barnett <javarrisb@gmail.con>
2021-12-18 20:39:37 +0100

    Employee test run

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,19 @@
+const Employee = require('../lib/Employee');
+
 test('creates an employee object', () => {
-    const employee = new Employee('Jim', 1, 'emp@test.com');
+    const employee = new Employee('Jim', '123', 'emp@test.com');
 
     expect(employee.name).toBe('Jim');
-    expect(employee.id).toBe(value);
-    expect(employee.email).toBe(value);
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('emp@test.com');
+    
+});
+
+test('retrieves employee properties', () => {
+    const employee = new Employee('Jim', '123', 'emp@test.com');
+
+    expect(employee.getName()).toBe('Jim');
+    expect(employee.getId()).toBe('123');
+    expect(employee.getEmail()).toBe('emp@test.com');
+    expect(employee.getRole()).toBe('Employee');
 });
